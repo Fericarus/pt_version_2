8eae43c6ce82554b75a471588b1dbe5fc6427373
Fernando Arriola Colunga <61210748+Fericarus@users.noreply.github.com>
2023-03-05 03:49:31 -0600

    Falta llenar los datos del grafico

--- vistas/administradores/administradores.php
+++ vistas/administradores/administradores.php
@@ -175,7 +175,9 @@
                     </div>
 
                     <div class="contenedor_detalles">
-                        <div class="contenedor_grafico"></div>
+                        <div class="contenedor_grafico">
+                            <canvas id="myChart"></canvas>
+                        </div>
                         <div class="contenedor_ultimos_clientes">
 
                             <!-- Titulo -->
@@ -233,3 +235,101 @@
 </html>
 
 <script src="https://kit.fontawesome.com/e5d8247fe1.js" crossorigin="anonymous"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+<script>
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: [
+                'Azcapotzalco',
+                'Álvaro Obregón',
+                'Benito Juárez',
+                'Coyoacán',
+                'Cuajimalpa de Morelos',
+                'Cuauhtémoc',
+                'Gustavo A. Madero',
+                'Iztacalco',
+                'Iztapalapa',
+                'La Magdalena Contreras',
+                'Miguel Hidalgo',
+                'Milpa Alta',
+                'Tlalpan',
+                'Tláhuac',
+                'Venustiano Carranza',
+                'Xochimilco'
+            ],
+            datasets: [{
+                label: 'Clientes por alcaldía',
+                data: [
+                    12,
+                    19,
+                    3,
+                    5,
+                    2,
+                    3,
+                    12,
+                    19,
+                    3,
+                    5,
+                    2,
+                    3,
+                    12,
+                    19,
+                    3,
+                    5
+                ],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                    'rgba(0, 128, 128, 0.2)',
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(184, 141, 141, 0.2)',
+                    'rgba(60, 144, 136, 0.2)',
+                    'rgba(241, 224, 90, 0.2)',
+                    'rgba(235, 64, 52, 0.2)',
+                    'rgba(170, 68, 101, 0.2)',
+                    'rgba(255, 127, 14, 0.2)',
+                    'rgba(84, 177, 255, 0.2)',
+                    'rgba(0, 216, 255, 0.2)',
+                    'rgba(16, 185, 129, 0.2)',
+                    'rgba(142, 100, 177, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)',
+                    'rgba(0, 128, 128, 1)',
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(184, 141, 141, 1)',
+                    'rgba(60, 144, 136, 1)',
+                    'rgba(241, 224, 90, 1)',
+                    'rgba(235, 64, 52, 1)',
+                    'rgba(170, 68, 101, 1)',
+                    'rgba(255, 127, 14, 1)',
+                    'rgba(84, 177, 255, 1)',
+                    'rgba(0, 216, 255, 1)',
+                    'rgba(16, 185, 129, 1)',
+                    'rgba(142, 100, 177, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+</script>
